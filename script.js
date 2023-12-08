f473f3eef365887bd89b665d00f298dc01fd6784
Maxwell Lima <49194750+Maxwellx96@users.noreply.github.com>
2023-12-08 16:26:37 -0300

    Finalização do Script.sj
    Finalizado script.js para adicionar class 'active' nos botões do sidebar

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const links = ['', 'index.html', 'alunos.html']
-const sidebar = ['.sidebar-dashboard', '.sidebar-dashboard', '.sidebar-alunos']
+const sidebar = [ '.sidebar-dashboard', '.sidebar-dashboard', '.sidebar-alunos']
 const currentURL = window.location.href
 
-for(i = 0; i < links.length -1; i++){
-    if(currentURL.includes(links[i]) == true){
+for(i = 0; i < links.length; i++){
+    if(currentURL.slice(22) == links[i]){
         let active = document.querySelector(sidebar[i])
         active.classList.add('active');
         console.log(links[i])
-        break;
+    }
+    else{
+        active = document.querySelector(sidebar[0])
     }
 }
